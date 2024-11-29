6a1da7037affa82949238f554e190ce12dc5f316
Ben Hunter <code@benhunter.me>
2024-11-28 21:02:40 -0600

    Add Copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,4 +45,21 @@ return {
   --     },
   --   },
   -- },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = { "Copilot" },
+    event = { "InsertEnter" },
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+      }
+    },
+    config = function ()
+      require("copilot").setup({
+        -- suggestion = { enabled = false },
+        -- panel = { enabled = false },
+      })
+    end,
+  },
 }
