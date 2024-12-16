5a95e847c150697e8701aa47ca9eec88ed40c3db
Ben Hunter <code@benhunter.me>
2024-12-15 23:07:24 -0600

    Add telescope keybind <leader>fs for lsp_document_symbols. Disable tab to 4 spaces.

--- lua/options.lua
+++ lua/options.lua
@@ -5,7 +5,8 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
-vim.o.tabstop = 4
-vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+-- Set tab to 4 spaces everywhere:
+-- vim.o.tabstop = 4
+-- vim.o.expandtab = true
+-- vim.o.softtabstop = 4
+-- vim.o.shiftwidth = 4
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,4 +68,32 @@ return {
       print("Copilot setup loaded")
     end,
   },
+
+  {
+    -- install with yarn or npm
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+
+--   {
+--     'nvim-telescope/telescope.nvim', tag = '0.1.8',
+-- -- or                              , branch = '0.1.x',
+--     dependencies = { 'nvim-lua/plenary.nvim' },
+--     config = function ()
+--       require('telescope').setup{
+--         defaults = {
+--           mappings = {
+--             n = {
+--               ["<leader>fs"] = require('telescope.builtin').lsp_document_symbols,
+--             },
+--           },
+--         }
+--       }
+--     end
+--   }
 }
