af0034866a770450cd5463a128dad043ae64918c
Ben Hunter <code@benhunter.me>
2026-09-07 00:36:24 -0500

    update mapping Space-ff to all files (same as Space-fa)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Make <leader>ff include hidden and ignored files, matching NvChad's <leader>fa.
+map("n", "<leader>ff", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", { desc = "telescope find all files" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", "<leader>fs", require('telescope.builtin').lsp_document_symbols, { desc = "telescope find lsp_document_symbols"})
